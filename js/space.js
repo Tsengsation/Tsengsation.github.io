9efdabeb9f57e4fe8c5b6f5dfb570d22684a4058
Jonathan Tseng <jonathanttseng@gmail.com>
2014-12-24 23:33:23 -0500

    mouse tracking to move space scope

--- js/space.js
+++ js/space.js
@@ -2,14 +2,15 @@
 //  Constants 
 // ---------------------------------------------------
 var scopeRadius = 125;
+var scopeDistancePerFrame = 10;
 
 var rayColor = 'white';
 var rayWidth = 2;
 var rayMinLength = 30;
 var rayMaxLength = 70;
 
-var rayDistancePerFrame = 15; 
-var numRaysPerFrame = 8;
+var rayDistancePerFrame = 20; 
+var numRaysPerFrame = 15;
 
 // ---------------------------------------------------
 //  Ray 
@@ -18,8 +19,8 @@ var Ray = function(length, angle) {
 	this.length = length;
 	this.angle = angle;
 	this.offset = new Point(rayDistancePerFrame * Math.cos(angle), rayDistancePerFrame * Math.sin(angle));
-	var from = new Point(view.center.x + scopeRadius * Math.cos(angle), view.center.y + scopeRadius * Math.sin(angle));
-	var to = new Point(view.center.x + (scopeRadius + length) * Math.cos(angle), view.center.y + (scopeRadius + length) * Math.sin(angle));
+	var from = new Point(viewCenter.x + scopeRadius * Math.cos(angle), viewCenter.y + scopeRadius * Math.sin(angle));
+	var to = new Point(viewCenter.x + (scopeRadius + length) * Math.cos(angle), viewCenter.y + (scopeRadius + length) * Math.sin(angle));
 	this.line = new Path.Line(from, to);
 	this.line.strokeColor = rayColor;
 	this.line.strokeWidth = rayWidth;
@@ -38,20 +39,45 @@ Ray.prototype.delete = function() {
 }
 
 // ---------------------------------------------------
-//  Code
+//  Initializations
 // ---------------------------------------------------
 var rays = [];
 var viewCenter = view.center;
-function onResize(event) {
+var goalViewCenter = view.center;
 
+// ---------------------------------------------------
+//  Events
+// ---------------------------------------------------
+function onFrame() {
+	updateScope();
+	createNewRays();
+	updateRays();
 }
 
-function onFrame() {
+function onMouseMove(event) {
+	goalViewCenter = event.point;
+}
+
+// ---------------------------------------------------
+//  Helpers
+// ---------------------------------------------------
+function updateScope() {
+	var difference = new Point(goalViewCenter.x - viewCenter.x, goalViewCenter.y - viewCenter.y);
+	var distance = Math.sqrt(Math.pow(difference.x, 2) + Math.pow(difference.y, 2));
+	if (distance != 0) {
+		var offset = new Point(scopeDistancePerFrame / distance * difference.x, scopeDistancePerFrame / distance * difference.y);
+		viewCenter = new Point(viewCenter.x + offset.x, viewCenter.y + offset.y);
+	}
+}
+
+function createNewRays() {
 	for (var i = 0; i < numRaysPerFrame; i++) {
 		var ray = new Ray(randomRayLength(), randomAngle());
 		rays.push(ray);
 	}
+}
 
+function updateRays() {
 	var raysNext = [];
 	for (var i = 0, l = rays.length; i < l; i++) {
 		if (rays[i].isVisible()) {
@@ -64,9 +90,6 @@ function onFrame() {
 	rays = raysNext;
 }
 
-// ---------------------------------------------------
-//  Helpers
-// ---------------------------------------------------
 function randomRayLength() {
 	return Math.random() * (rayMaxLength - rayMinLength) + rayMinLength;
 }
